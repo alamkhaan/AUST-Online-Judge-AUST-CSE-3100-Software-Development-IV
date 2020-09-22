bbc2f45c800ba284ef5b84814bbe7d747c9b751c
Alam Khan <42060114+alamkhaan@users.noreply.github.com>
2020-09-22 21:49:11 +0600

    Update register.php

--- AustOJ/register.php
+++ AustOJ/register.php
@@ -1,4 +1,12 @@
 <?php
+
+
+    session_start();
+	if(isset($_SESSION["username"]))
+	{
+		header("location:index.php");
+		exit();
+	}
     include 'notloggedin.php';
 
     $conn = new mysqli("localhost", "root","", "170204084");
